0c5c578618f4ab8ee881deba3be0dfccfb9696e6
24aft-kawasaki <24aft.kawasaki@gmail.com>
2025-12-04 04:18:20 +0000

    Release: v0.1.0 and Formula.rb version update
    - Update `brew-offload.rb` formula to use v0.1.0 tarball and sha256.

--- brew-offload.rb
+++ brew-offload.rb
@@ -1,8 +1,8 @@
 class BrewOffload < Formula
   desc "Homebrew wrapper to offload some formulae"
   homepage "https://github.com/24aft-kawasaki/homebrew-offload"
-  url "https://github.com/24aft-kawasaki/homebrew-offload/archive/refs/tags/v0.0.0.tar.gz"
-  sha256 "2bdc19c1d1e9ff9f682dc5e62c6f1548ee290a3fdf66f934d84bd642a55ae36d"
+  url "https://github.com/24aft-kawasaki/homebrew-offload/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "a65e15ea8fa306461c7c021bf734f24fda6a18557686f85756b25e5159735b76"
   license ""
 
   depends_on "python@3.13"
