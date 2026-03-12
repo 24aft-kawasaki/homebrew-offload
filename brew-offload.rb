05d07a17e1bf6d5c7a60a801bb6e891d4380ab74
24aft-kawasaki <24aft.kawasaki@gmail.com>
2026-03-12 03:00:34 +0000

    とりあえずテストは通った

--- brew-offload.rb
+++ brew-offload.rb
@@ -1,14 +1,16 @@
 class BrewOffload < Formula
   desc "Homebrew wrapper to offload some formulae"
   homepage "https://github.com/24aft-kawasaki/homebrew-offload"
-  url "https://github.com/24aft-kawasaki/homebrew-offload/archive/refs/tags/v0.4.0-test.tar.gz"
-  sha256 "5db1474d0b3fae3ca9956731269e9c787c99a4b178e1ffcc04da264eec4f3779"
+  url "file:///tmp/homebrew-offload-0.0.1.tar.gz"
+  sha256 "6bb6a9c38c0c64db049c6ac30e2a1b29c0a3c177c653a49813338641ed29d68c"
   license ""
 
   # Uses system Python 3.14+ - version check is performed at runtime
 
   def install
-    bin.install "bin/brew-offload"
+    libexec.install Dir["bin/*"]
+    (bin/"brew-offload").write_env_script libexec/"brew-offload",
+      :PATH => "#{Formula["python@3.14"].opt_bin}:$PATH"
     etc.install "testenv/etc/brew-offload"
     (etc/"brew-offload").install "etc/brew-wrap"
     zsh_completion.install "etc/_brew-offload"
